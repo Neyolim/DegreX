3fb90549688eac6fdac6b12af6bf380625e0ebc4
Neyolim <neyoollim@gmail.com>
2025-08-10 18:12:15 +0545

    add react scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
@@ -8,28 +8,31 @@ import Campus from './components/Campus/Campus';
 import Testimonials from './components/Testimonials/Testimonials';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/footer';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
 
 
 export default function App() {
+
+  const [playState,setPlayState] = useState(false);
+
   return (
     <div>
       <Navbar />
       <Hero />
       <div className="container">
-        <Title subTitle='Our PROGRAM' title='What We Offer' />
+        <Title subTitle="Our PROGRAM" title="What We Offer" />
         <Programs />
-        <About />
-         <Title subTitle='Gallery' title='Campus Photos' />
-         <Campus/>
-          <Title subTitle='TESTIMONIALS' title='What Students Says' />
-          <Testimonials/>
-           <Title subTitle='Contact Us' title='Get in Touch' />
-           <Contact />
-           <Footer />
+        <About setPlayState={setPlayState} />
+        <Title subTitle="Gallery" title="Campus Photos" />
+        <Campus />
+        <Title subTitle="TESTIMONIALS" title="What Students Says" />
+        <Testimonials />
+        <Title subTitle="Contact Us" title="Get in Touch" />
+        <Contact />
+        <Footer />
       </div>
-
-
+      <VideoPlayer playState={playState} setPlayState={setPlayState} />
     </div>
-
-  )
+  );
 }
+
